d6114a3024ff207b7039fa332636fb040a561cd4
Gonzalo Martinez <gonzalo.martinez@litermi.com>
2023-03-23 13:51:30 -0300

    Harrrrmoso

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
-    echo $_ENV["IP_DB"] . "<br />";
-    echo $_ENV["USER_DB"] . "<br />";
-    echo $_ENV["NAME_DB"] . "<br />";
+
+    $conexion = new mysqli(
+        getenv("IP_DB"),
+        getenv("USER_DB"),
+        getenv("PASSWORD_DB"),
+        getenv("NAME_DB")
+    );
+
+    echo "Linda demo. Cada vez que cargue esta pagina, inserta un registro random en la tabla. <br /><br />";
+    $sql = "INSERT INTO dato(dato) VALUES(RAND())";
+    echo "Estos son los datos que hay en la tabla: <br />";
+    $conexion -> query($sql);
+
+    $sql = "SELECT id,dato FROM dato";
+
+    $filas = $conexion -> query($sql);
+    echo "<pre>";
+    echo "ID\tDato\n";
+    foreach($filas as $fila){
+        echo $fila["id"]. "\t" . $fila["dato"]. "\n";
+    }
+    echo "</pre>";
